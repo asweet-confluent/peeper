3c202f351ea372acf067b7d9995cdd557ff61c38
Alex Sweet <asweet@confluent.io>
2026-06-11 11:40:52 -0700

    v1.2.2

--- packages/renderer/src/components/PreferencesModal.tsx
+++ packages/renderer/src/components/PreferencesModal.tsx
@@ -306,7 +306,7 @@ const PreferencesModal: React.FC<PreferencesModalProps> = ({
               <p className="preference-description">
                 Peeper - GitHub Notifications Desktop App
                 <br />
-                Version 1.2.1
+                Version 1.2.2
                 <br />
                 Built with Electron and React
               </p>
